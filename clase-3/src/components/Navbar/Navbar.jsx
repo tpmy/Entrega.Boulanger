d2bba9cc874ee892ce243a54b3ae9675daa4ae23
tpmy <tomitoboulanger@hotmail.com>
2022-10-27 00:57:06 -0300

    Entrega

--- clase-3/src/components/Navbar/Navbar.jsx
+++ clase-3/src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
         <li className="nav-item">
           <a className="nav-link" href="#">Link</a>
         </li>
-
+        <Dropdown/>
       </ul>
         <FormBusqueda busqueda={"Buscar Productos"}/>
     </div>
